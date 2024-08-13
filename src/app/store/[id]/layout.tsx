35d88f393307c5675434b18202070ed924a0d1bc
jangth0655 <jangth0655@gmail.com>
2024-08-13 15:26:44 +0000

    image test

--- src/app/store/[id]/layout.tsx
+++ src/app/store/[id]/layout.tsx
@@ -3,12 +3,12 @@ import Script from 'next/script';
 export default function StoreDetailLayout({ children }: { children: React.ReactNode }) {
   return (
     <section>
-      <Script
+      {/* <Script
         id="naver-map-script"
         type="text/javascript"
         src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env
           .NEXT_PUBLIC_NMAP_KEY!}&submodules=geocoder`}
-      />
+      /> */}
       {children}
     </section>
   );
